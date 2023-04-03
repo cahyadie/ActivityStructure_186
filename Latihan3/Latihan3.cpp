e89e1a5fe2209f28e0639319bd60d69cf13845f2
cahyadie <cahyadiilham12@gmail.com>
2023-04-03 11:18:00 +0700

    perulngan pertama

--- Latihan3/Latihan3.cpp
+++ Latihan3/Latihan3.cpp
@@ -14,3 +14,22 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukan NIM : ";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukan Nama : ";
+		cin.getline(mhs[i].Nama, 25);
+		cout << "Alamat : ";
+		cout << "\n\tMasukan desa : ";
+		cin.getline(mhs[i].alamat.Desa, 20);
+		cout << "\tMasukan Kota : ";
+		cin.getline(mhs[i].alamat.Kota, 20);
+		cout << "masukan umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+}
