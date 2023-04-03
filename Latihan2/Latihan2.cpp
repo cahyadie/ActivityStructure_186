4093e163d1d62f8bc545d0798a4692e6e7833322
cahyadie <cahyadiilham12@gmail.com>
2023-04-03 10:55:06 +0700

    struktur dalam struktur

--- Latihan2/Latihan2.cpp
+++ Latihan2/Latihan2.cpp
@@ -14,3 +14,17 @@ struct mahasiswa
 	AlamatDetail Alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+
+	cout << "Maukan NIM : ";
+	cin >> mhs.NIM;
+	cout << "Maukan Nama : ";
+	cin >> mhs.Nama;
+	cout << "Alamat : ";
+	cout << "\n\tMasukan Desa : ";
+	cin >> mhs.Alamat.Desa;
+	cout << "\tMasukan Kota : ";
+	cin >> mhs.Alamat.Kota;
+}
