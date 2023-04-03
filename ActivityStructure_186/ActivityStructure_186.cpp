334b65f068018e8a5ef28d2cb111581b5f9e7a0e
cahyadie <cahyadiilham12@gmail.com>
2023-04-03 10:21:02 +0700

    input variabel

--- ActivityStructure_186/ActivityStructure_186.cpp
+++ ActivityStructure_186/ActivityStructure_186.cpp
@@ -9,4 +9,20 @@ struct mahasiswa
 	int umur;
 };
 
+int mainn() {
+	mahasiswa mhs1, mhs2;
+	mhs1.NIM = "20220140019;";
+	mhs1.Nama = "Ardian Mulya";
+	mhs1.alamat = "Wonosobo";
+	mhs1.umur = 20;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama : ";
+	cin >> mhs2.Nama;
+	cout << "Maukan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
+
 }
